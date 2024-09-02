8a23de1d6b78f0bd2554c6661f303452dfd93ce4
Joaquín Ruiz <joaquinruizbueno@gmail.com>
2024-09-02 19:42:19 +0100

    chore: Refactor password handling, fix form submission, and update password constants to use camel case

--- pages/masterclass-9-errores-autocad-estas-dentro.js
+++ pages/masterclass-9-errores-autocad-estas-dentro.js
@@ -17,7 +17,7 @@ const MasterClass9ErroresMasFrecuentesEstasDentro = () => {
       <div className="bg-stone-50 p-4 w-full my-8 px-4 grid place-content-center leading-10">
         {password !== PASWORDS.PASWORD_1 ? (
           <>
-            <h1 className="text-5xl font-playfair">
+            <h1 className="text-5xl font-playfair text-center">
               Acceso Masterclass 9 Errores AutoCAD
             </h1>
             <br />
@@ -31,17 +31,67 @@ const MasterClass9ErroresMasFrecuentesEstasDentro = () => {
             </h2>
           </>
         ) : (
-          <div className="p-4 sm:p-24">
+          <div className="p-4 sm:p-36">
             <h1 className="text-5xl font-playfair text-center pb-24">
-              Masterclass 9 Errores más frecuentes en AutoCAD
+              Masterclass Los 9 Problemas más Frecuentes en AutoCad.
             </h1>
-            <div className="w-full flex justify-center flex-col space-y-24">
+
+            <br></br>
+            <br></br>
+
+            <p>
+              <i>
+                En esta página damos muchos recursos, no solo la masterclass.
+                Por favor,
+                <b>
+                  {" "}
+                  lee hasta el final, hay información importante y no me
+                  gustaría que te perdieras nada.
+                </b>{" "}
+              </i>
+            </p>
+            <br></br>
+            <br></br>
+            <br></br>
+            <div className=" mb-36">
+              <h2 className="font-alternate text-3xl font-bold leading-[52px]">
+                - La Masterclass.
+              </h2>
+              <br></br>
+              <p>
+                <i>
+                  Nota importante: esta masterclass contiene mucha información,
+                  muchos detalles. Puedes volver a verla tantas veces como
+                  quieras, te recomendamos que lo hagas.
+                </i>
+              </p>
+              <br></br>
+
               <iframe
                 src="https://drive.google.com/file/d/1O7yy0XXGoqNcedAvUPuT-K_499hP-lE5/preview"
                 width="768px"
                 height="432px"
                 allowFullScreen
               ></iframe>
+            </div>
+
+            <div className=" mb-36">
+              <h2 className="font-alternate text-3xl font-bold leading-[42px]">
+                - Bonus - Plantilla de Comandos de AutoCad (para que diseñes
+                como un pro).
+              </h2>
+              <br></br>
+              <p>
+                Hemos decidido regalarte este bonus de bienvenida, pero no
+                queremos que pienses que tiene poco valor por el hecho de que
+                sea un regalo. Es más, es seguro que lo pondremos a la venta más
+                adelante.
+              </p>
+              <p>
+                En el minuto 08:05 de la Masterclass te explico cómo usarlo y
+                todo lo que necesitas saber sobre este bonus.{" "}
+              </p>
+              <br></br>
 
               <a
                 href={
@@ -52,9 +102,64 @@ const MasterClass9ErroresMasFrecuentesEstasDentro = () => {
                 <button
                   className={`w-full font-playfair bg-bid-wine tracking-widest text-stone-50  py-3 text-2xl  duration-150 hover:opacity-90 `}
                 >
-                  Bonus - Los comandos más usados en Autocad
+                  Bonus - Plantilla comandos AutoCad
                 </button>
               </a>
+            </div>
+
+            <div className=" mb-36">
+              <h2 className="font-alternate text-3xl font-bold leading-[42px]">
+                - Recomendaciones para ver la Masterclass.
+              </h2>
+              <br></br>
+              <ol className="px-6">
+                <li>
+                  Recomendamos ver la masterclass por orden. La masterclass es
+                  muy progresiva, es decir, si ves el Problema 9 sin ver antes
+                  el Problema 3, no lo entenderás bien y no podrás solucionarlo.
+                  No seas ansias y ve paso a paso.
+                </li>
+                <li>
+                  Recomendamos ver los 9 problemas del tirón. Luego te
+                  recomendamos que lo vuelvas a ver con tu autocad abierto, y te
+                  vayas parando en todos los errores relevantes que tú cometas
+                  para poner en práctica sus soluciones. Es una hora larga de
+                  masterclass de alto nivel sobre dibujo en el sector de la
+                  construcción y te vas a encontrar con bastantes detalles
+                  interesantes. Así que para el vídeo, ponlo en práctica y
+                  sigues. Hay mucha información.
+                </li>
+              </ol>
+            </div>
+
+            <div className="">
+              <h2 className="font-alternate text-3xl font-bold leading-[42px]">
+                - Índice de la Masterclass.
+              </h2>
+              <br></br>
+              <p>
+                Como te decía antes, recomendamos ver la masterclass por orden.
+                Pero si luego quieres recordar alguna cosa, te vendrá bien tener
+                un índice del contenido:{" "}
+              </p>
+              <p>
+                Problema 1: No saber escalar ni redibujar bien un pdf | 02:15
+              </p>
+              <p>
+                Problema 2: La verdadera razón de que no dibujes rápido en
+                AutoCad | 08:05
+              </p>
+              <p>Problema 3: En qué unidades trabajas. | 13:20</p>
+              <p>Problema 4: No seas hortera. | 20:36</p>
+              <p>
+                Problema 5: En qué escala vas a representar tus planos. | 27:20
+              </p>
+              <p>
+                Problema 6: La marranada de los textos en los planos. | 34:11
+              </p>
+              <p>Problema 7: Por qué mis cotas se leen mal. | 40:17</p>
+              <p>Problema 8: Por favor, usa bien tus capas. | 47:48</p>
+              <p>Problema 9: Imprime como un pro. | 53:51</p>
             </div>
           </div>
         )}
--- pages/taller-proyecto-ejecucion-vivienda-vlc-estas-dentro.js
+++ pages/taller-proyecto-ejecucion-vivienda-vlc-estas-dentro.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import FormAccesContent from "../components/FormAccesContent";
 
-const MasterClass9ErroresMasFrecuentesEstasDentro = () => {
+const TallerProyectoEjecucionViviendaValenciaDentro = () => {
   const [password, setPassword] = useState("");
 
   const PASWORDS = {
@@ -11,14 +11,14 @@ const MasterClass9ErroresMasFrecuentesEstasDentro = () => {
   return (
     <div
       className={`w-full ${
-        password === PASWORDS.PASWORD_1 ? "h-[90vh]" : ""
+        password !== PASWORDS.PASWORD_1 ? "h-[90vh]" : ""
       } max-w-4xl place-content-center font-playfair`}
     >
       <div className="bg-stone-50 p-4 w-full my-8 px-4 grid place-content-center leading-10">
-        {password === PASWORDS.PASWORD_1 ? (
+        {password !== PASWORDS.PASWORD_1 ? (
           <>
             <h1 className="text-5xl font-playfair text-center">
-              Acceso Taller Proyecto de ejecución vivienda Valencia
+              Acceso Taller en Directo Proyecto de Ejecución Vivienda Valencia
             </h1>
             <br />
             <FormAccesContent
@@ -31,23 +31,49 @@ const MasterClass9ErroresMasFrecuentesEstasDentro = () => {
             </h2>
           </>
         ) : (
-          <div className="p-4 sm:p-24">
+          <div className="p-4 sm:px-8 sm:pt-36">
             <h1 className="text-5xl font-playfair text-center pb-24">
-              Masterclass 9 Errores más frecuentes en AutoCAD
+              Taller en Directo Proyecto de Ejecución Vivienda Valencia
             </h1>
 
-            <div className="border-4 rounded border-bid-wine p-4 text-center">
-              <p className="pb-6 ">
-                El directo esl el día 5 de Octubre a las 18:00 hora peninsular
-                española.
-                <br />
-                Clicka en el botón para acceder al directo.
-              </p>
-              <p className="italic text-xs pb-12">
-                *Una vez termine el directo podrás acceder aquí a la grabación
-              </p>
+            <br></br>
+            <br></br>
 
-              <a href={"https://www.youtube.es"} target="_blank">
+            <p>
+              <i>
+                En esta página damos muchos recursos, no solo el taller. Por
+                favor,
+                <b>
+                  {" "}
+                  lee hasta el final, hay información importante y no me
+                  gustaría que te perdieras nada.
+                </b>{" "}
+              </i>
+            </p>
+            <br></br>
+            <br></br>
+            <br></br>
+            <div className=" mb-36">
+              <h2 className="font-alternate text-3xl font-bold leading-[52px]">
+                - El Taller.
+              </h2>
+              <br></br>
+              <p>
+                El directo es el día 5 de Octubre a las 18:00 (Madrid, España).
+              </p>
+              <p>
+                Cuando llegue la hora del directo, clica en el botón de abajo
+                para acceder.{" "}
+              </p>
+              <p>
+                <i>
+                  * Una vez termine el directo, colgaremos aquí la grabación
+                  para que puedas volver a verlo tantas veces como quieras.
+                </i>
+              </p>
+              <br></br>
+
+              <a href={""} target="_blank">
                 <button
                   className={`w-full font-playfair bg-bid-wine tracking-widest text-stone-50  py-3 text-2xl  duration-150 hover:opacity-90 `}
                 >
@@ -56,16 +82,116 @@ const MasterClass9ErroresMasFrecuentesEstasDentro = () => {
               </a>
             </div>
 
-            <div className="w-full flex flex-col items-center pt-24">
-              <p className="text-5xl font-playfair text-center pb-8">
-                Bonus - Audio privado
-              </p>{" "}
-              <audio controls controlsList="nodownload">
-                <source
-                  src="https://res.cloudinary.com/dh2hwwzpy/video/upload/v1725049570/ZOOM0061_xznawj.mp3"
-                  type="audio/mp3"
-                ></source>
-              </audio>
+            <div className=" mb-36">
+              <h2 className="font-alternate text-3xl font-bold leading-[42px]">
+                - Bonus - Audio Privado.
+              </h2>
+              <br></br>
+              <p>
+                Hemos decidido darte regalarte este audio de bienvenida, pero no
+                queremos que pienses que tiene poco valor por el hecho de que
+                sea un regalo.
+              </p>
+              <p>Es más, es seguro que lo pondremos a la venta más adelante.</p>
+              <p>
+                Si aplicas lo que te contamos en el audio (trabajes por cuenta
+                ajena o por cuenta propia), amortizarás con creces el precio
+                completo de este taller. Escúchalo tantas veces como necesites.
+              </p>
+              <br></br>
+
+              <div className="flex justify-center">
+                <audio controls controlsList="nodownload">
+                  <source
+                    src="https://res.cloudinary.com......"
+                    type="audio/mp3"
+                  ></source>
+                </audio>
+              </div>
+            </div>
+
+            <div className=" mb-36">
+              <h2 className="font-alternate text-3xl font-bold leading-[42px]">
+                - Proyecto de ejecución de interiorismo Vivienda Valencia en
+                pdf.
+              </h2>
+              <br></br>
+              <p>
+                <i>
+                  Estará disponible unos días antes del directo. Te avisaremos
+                  por email cuando puedas acceder a él. No lo damos antes para
+                  no hacer spoiler.
+                </i>
+              </p>
+            </div>
+
+            <div className=" mb-36">
+              <h2 className="font-alternate text-3xl font-bold leading-[42px]">
+                - Recomendaciones para el directo.
+              </h2>
+              <br></br>
+              <ol className="px-6">
+                <li>
+                  Recomendamos imprimir el pdf del Proyecto de Ejecución para
+                  que puedas seguir mejor el directo y hacer anotaciones
+                  directamente encima de él. Por eso lo enviaremos unos días
+                  antes, para que tengas tiempo para imprimirlo.
+                </li>
+                <li>
+                  Si no lo imprimes porque eres como Laura que se organiza todo
+                  en digital, ten en cuenta prepararte el pdf en tu tablet o
+                  dispositivo donde puedas tomar notas en el documento.
+                </li>
+                <li>
+                  Coge papel y boli, o tu app de notas, y si durante el taller
+                  te entra una duda, apúntala. A todos nos ha pasado que nos
+                  asalta una pregunta pero a la hora ya se ha olvidado. Tienes
+                  la oportunidad de resolver tus dudas con nosotros, no la
+                  desaproveches.
+                </li>
+              </ol>
+            </div>
+
+            <div className="">
+              <h2 className="font-alternate text-3xl font-bold leading-[42px] pb-8">
+                - Sesión de dudas post-directo.
+              </h2>
+              <h2 className="font-alternate text-2xl font-bold text-center">
+                Cómo enviarnos tus dudas
+              </h2>
+
+              <br></br>
+
+              <ol>
+                <li>
+                  <b>
+                    Podrás enviarnos tus dudas por email hasta el día 19 de
+                    octubre a las 23:59 (Madrid, España).
+                  </b>{" "}
+                  Pasado ese día, ya no responderemos dudas.{" "}
+                </li>
+                <li>
+                  <b>
+                    El email deberá tener el asunto: DUDAS PARA NO CAGARLA EN LA
+                    OBRA.
+                  </b>{" "}
+                  Es decir, cuando nos pongamos a responder filtraremos los
+                  emails con ese asunto, si no has puesto ese asunto, tu duda no
+                  nos aparecerá y no la podremos responder.{" "}
+                </li>
+                <li>
+                  Responderemos dudas que tengan que ver con el taller. Me
+                  explico. Si nos preguntas cómo dibujar un salón para el
+                  proyecto de tu tía-abuela… Pues no.
+                </li>
+                <li>
+                  Una semana después, el día 26 de octubre 2024, publicaremos
+                  las respuestas aquí. O sea, no solo contestaremos a tu duda,
+                  si no que podrás ver las preguntas de otras personas y que
+                  seguramente también te ayuden. Como siempre, te avisaremos por
+                  email.{" "}
+                </li>
+              </ol>
             </div>
           </div>
         )}
@@ -74,4 +200,4 @@ const MasterClass9ErroresMasFrecuentesEstasDentro = () => {
   );
 };
 
-export default MasterClass9ErroresMasFrecuentesEstasDentro;
+export default TallerProyectoEjecucionViviendaValenciaDentro;
