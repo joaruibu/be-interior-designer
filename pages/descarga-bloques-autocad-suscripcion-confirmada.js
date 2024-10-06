f569de12b8fb7a3c262e2b3c5945d5b3455facc9
Joaquín Ruiz <joaquinruizbueno@gmail.com>
2024-10-06 09:09:52 +0100

    chore: remove page

--- pages/descarga-bloques-autocad-suscripcion-confirmada.js
+++ pages/descarga-bloques-autocad-suscripcion-confirmada.js
@@ -3,7 +3,7 @@ import React from "react";
 const DescargaBloquesAutocadSuscripcionConfirmada = () => {
   return (
     <div className="w-full h-[90vh] grid place-content-center font-playfair">
-      <div className="  bg-stone-50 p-4  my-8  px-4 grid place-content-center leading-10">
+      <div className=" bg-stone-50 p-4  my-8  px-4 grid place-content-center leading-10">
         <h1 className="text-5xl font-bigilla">Bloques enviados </h1>
         <br></br>
         <p>
@@ -12,8 +12,8 @@ const DescargaBloquesAutocadSuscripcionConfirmada = () => {
           </b>
         </p>
         <p>
-          Puede puede tardar hasta 10 minutos en llegar o que esté en spam, en
-          ese caso añade la dirección info@beinteriordesginer.com a tu lista de
+          Puede tardar hasta 10 minutos en llegar o que esté en spam, en ese
+          caso añade la dirección info@beinteriordesginer.com a tu lista de
           contactos.
         </p>
         <h2 className=" text-bid-wine font-bigilla font-bold">
